14f2d3531ff6b0bc2d0e2e3e953bb24abf7a8939
StarFishPRIME <76632198+kyleb1515@users.noreply.github.com>
2025-01-07 00:40:54 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
-from solana_sdk.rpc.api import Client  # Changed this line
+from solana.rpc.api import Client
 import time
 import os
 from dotenv import load_dotenv
